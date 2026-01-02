b959f64944a486ca201c0a25291cb423f3b231a1
songulgs190560-dev <songulgs190560@gmail.com>
2026-01-02 18:17:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ HTML_TEMPLATE = """
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Music Discovery</title>
+    <title>Songül Yağcı</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
     <style>
         :root {
